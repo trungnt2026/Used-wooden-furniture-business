7a619d7da28796c6cd586c9bb1ebdec2e18600fc
Thanh Trung <nguyenthanhtrung22121999@gmail.com>
2026-06-11 17:59:37 +0700

    update cart

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+// Tim san pham
 let searchInput = document.getElementById('searchInput');
 let products = document.querySelectorAll('.product');
 
@@ -9,10 +10,36 @@ searchInput.addEventListener("keyup", function() {
 
         if (name.includes(keyword)) {
             product.style.display = "block";
-
-        }else {
+        } else {
             product.style.display = "none";
         }
-
     });
 });
+
+// gio hang
+
+let cart = [];
+let total = 0;
+
+function addToCart(name, price) {
+
+    cart.push ({
+        name: name,
+        price: price
+    });
+
+    total = total + price;
+
+    document.getElementById("cartCount").innerText = cart.length;
+
+    let cartItems = document.getElementById("cartItems");
+
+    let li = document.createElement("li");
+    li.innerText = name + " - " + price.toLocaleString() + "đ";
+
+    cartItems.appendChild(li);
+
+    document.getElementById("totalPrice").innerText = total.toLocaleString();
+
+    alert("Đã thêm " + name + " vào giỏ hàng!");
+}
